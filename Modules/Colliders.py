a7e49bd0d95f881c757479a304d6e49a077d13b8
Skye Terran Stevenson <terransky1@gmail.com>
2018-12-09 18:27:43 -0800

    Blocked out colliders functionality

--- Modules/Colliders.py
+++ Modules/Colliders.py
@@ -1,6 +1,26 @@
 import maya.cmds as cmds
 import maya.mel as mel
+import sys
 
-#polyCylinder -r 0.25 -h 1 -sx 8 -sy 1 -sz 1 -ax 0 1 0 -rcp 0 -cuv 3 -ch 1;
+# alert user function
+def alert(message):
+    sys.stdout.write("Skorge says: " + message)
 
-# create capsule collider
+meshName = ""
+
+def createCollider(colliderType):
+	if (meshName != ""):
+		if (colliderType = "box"):
+			# create box collider
+			cmds.polyCube(name = "UBX_{}_#".format(meshName), w = 0.5, h = 0.5, d = 0.5, sx = 1, sy = 1, sz = 1, ax = [0, 1, 0])
+		if (colliderType = "capsule"):
+			# create capsule collider
+			cmds.polyCylinder(name = "UCP_{}_#".format(meshName), r = 0.25, h = 1, sx = 8, sy = 1, sz = 1, ax = [0, 1, 0])
+		if (colliderType = "sphere"):
+			# create sphere collider
+			cmds.polySphere(name = "USP_{}_#".format(meshName), r = 0.5, sx = 8, sy = 8, ax = [0, 1, 0])
+		if (colliderType = "convex"):
+			# create convex collider
+			cmds.polyCube(name = "UCX_{}_#".format(meshName), w = 0.5, h = 0.5, d = 0.5, sx = 1, sy = 1, sz = 1, ax = [0, 1, 0])
+	else:
+		alert("No naming convention set. Select a visible mesh and press 'Get mesh name' or type one in.")
