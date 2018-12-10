1bdca40628675c605e73527e0f8ae325f549a384
Skye Terran Stevenson <terransky1@gmail.com>
2018-12-09 19:47:01 -0800

    Improved collider behavior
    Now, colliders will be produced at the center of the bounding box of the visible mesh

--- Modules/Colliders.py
+++ Modules/Colliders.py
@@ -8,6 +8,7 @@ def alert(message):
 
 def createCollider(colliderType, meshName):
 	if (meshName != ""):
+		# make the correct collider primitive
 		if (colliderType == "box"):
 			# create box collider
 			cmds.polyCube(name = "UBX_{}_#".format(meshName), w = 0.5, h = 0.5, d = 0.5, sx = 1, sy = 1, sz = 1, ax = [0, 1, 0])
@@ -20,5 +21,16 @@ def createCollider(colliderType, meshName):
 		if (colliderType == "convex"):
 			# create convex collider
 			cmds.polyCube(name = "UCX_{}_#".format(meshName), w = 0.5, h = 0.5, d = 0.5, sx = 1, sy = 1, sz = 1, ax = [0, 1, 0])
+
+		# if the mesh name correlates to a real object in the scene, move the primitive to the center of its bounding box!
+		if cmds.objExists(meshName):
+			# queries the bounding box of the object in world space
+			bb = cmds.xform(meshName, q = True, bb = True, ws = True)
+
+			# averages x, y and z bounds to find center point in world space
+			center = ((bb[0]+bb[3])/2,(bb[1]+bb[4])/2,(bb[2]+bb[5])/2)
+
+			# move the collider we just made to that center
+			cmds.xform(ws = True, t = center)
 	else:
 		alert("No naming convention set. Select a visible mesh and press 'Get mesh name' or type one in.")
